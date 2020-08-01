98d05072d32fcc14295d41b22cc2fcd834d81f0a
Admin <osipovivan23@gmail.com>
2020-08-01 23:31:07 +0300

    Add SingleFragmentActivity

---
+++ app/src/main/java/ru/osipov/projectbehance/common/RefreshOwner.java
@@ -0,0 +1,5 @@
+package ru.osipov.projectbehance.common;
+
+public interface RefreshOwner {
+    void setRefreshState(boolean refreshing);
+}
---
+++ app/src/main/java/ru/osipov/projectbehance/common/Refreshable.java
@@ -0,0 +1,5 @@
+package ru.osipov.projectbehance.common;
+
+public interface Refreshable {
+    void onRefreshData();
+}
